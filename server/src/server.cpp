6eb1c94e857f602dbaa44b77be89beb690e794a1
nblanque <nblanque@cs-122>
2021-11-29 22:05:52 -0500

    Created skeleton for server program

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -6,9 +6,30 @@
 #include <vector>
 
 #include "net.h"
+#include "image.h"
 
+//Arguments order: image file name, image out file name, function name (blur, threshold, or upsample), chunks, threads
 int main(int argc, char* argv[]){
 
-    
+    //Array of loaded images (chopped up)
+    //Pop chunk off as it's sent
+    vector<Image> chunks;
 
+    //TODO: Split up into chunks and push onto vector
+
+    //Array of booleans for assigned chunks
+    vector<bool> assigned;
+
+    //Array of availability of workers
+    vector<bool> workerAvailability;
+
+    //Array of image results for chunks;
+    vector<Image> results;
+    
+    //Use threads to open connections and send stuff to workers + recieve results
+    //Handle specific function reqs like padding for blur and sum for threshold
+    //Use mutex for results
+
+    //Aggregate results into an output image
+    
 }
