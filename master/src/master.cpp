e23c6adbf985b8bb650efaf361937dc337b4759b
nblanque <nblanque@cs-122>
2021-12-01 00:28:51 -0500

    Added some code to master for hotdogging

--- master/src/master.cpp
+++ master/src/master.cpp
@@ -1,3 +1,10 @@
+//#define STB_IMAGE_IMPLEMENTATION
+//#include "stb_image.h"
+
+//#define STB_IMAGE_WRITE_IMPLEMENTATION
+//#include "stb_image_write.h"
+
+#include <iostream>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -8,19 +15,13 @@
 #include "net.h"
 #include "image.h"
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-
 //Arguments order: image file name, image out file name, function name (blur, threshold, or upsample), chunks, threads
 int main(int argc, char* argv[]){
 
     //Constants go here up top
-    const int n = 1;
+    int n = 1;
     const int workers = 4; //Assuming 4 workers for now
-    const int hotdogSections = argv[4];
+    const int hotdogSections = std::stoi(argv[4]);
 
     //Array of loaded images (chopped up)
     //Pop chunk off as it's sent
@@ -31,15 +32,23 @@ int main(int argc, char* argv[]){
     int h;
     unsigned char * data = stbi_load(argv[1], &w, &h, &n, 1);
 
-    for(int chunk = 0; chunk < hotdogSections; i++){
+    std::cout << hotdogSections << std::endl;
 
-        unsigned char * hotdogData = new char[(w * h) / hotdogSections];
+    for(int chunk = 0; chunk < hotdogSections; chunk++){
 
-        for(int x = 0; x < w / 8; x++){
+        //std::cout << (w * h) / hotdogSections << std::endl;
 
-            for(int y = 0; y < h; y++){
+        unsigned char * hotdogData = new unsigned char[(w * h) / hotdogSections];
 
-                hotdogData[y * (w / 8) * chunk + x] = data[y * (w / hotdogSections) * chunk + x];
+        for(int i = chunk * (w / hotdogSections); i < (chunk + 1) * (w / hotdogSections); i++){
+
+            for(int x = 0; x < w / hotdogSections; x++){
+
+                for(int y = 0; y < h; y++){
+
+                    hotdogData[y * (w / hotdogSections) + x] = data[y * w + i];
+
+                }
 
             }
 
@@ -50,7 +59,7 @@ int main(int argc, char* argv[]){
     }
 
     //Testing of writing of chunk
-    stbi_write_png(argv[2] + "chunk1.png", w / hotdogSections, h, n, chunks.at(0), (w / hotdogSections) * n);
+    stbi_write_png("../data/chunk1.png", w / hotdogSections, h, n, chunks.at(0), (w / hotdogSections) * n);
 
     //Array of booleans for assigned chunks
     std::vector<bool> assigned;
