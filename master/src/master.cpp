aaad6c9a0551c3c817a66ff64287f6437949145d
Nathaniel K. Blanquel <nblanque@fandm.edu>
2021-12-17 21:13:34 -0500

    Update master.cpp

--- master/src/master.cpp
+++ master/src/master.cpp
@@ -16,10 +16,13 @@ namespace kn = kissnet;
 #define METADATA_MAX_LENGTH 55
 #define IMG_DATA_BUFF_SIZE 4096
 
+//to run asyncronously, receives "done" messages from the workers and sets their status accordingly
 void msg_recv(std::vector<kn::tcp_socket> &workers, int i, std::vector<bool> &assigned, unsigned char *chunk, std::vector<unsigned char *> &chunks, std::vector<unsigned char *> &processed)
 {
+    //set the buffer for receiving data from worker
     kn::buffer<METADATA_MAX_LENGTH> metadata_buff;
 
+    //receive data from worker
     auto [size, status] = workers.at(i).recv(metadata_buff);
 
     while (size <= 0)
@@ -27,13 +30,12 @@ void msg_recv(std::vector<kn::tcp_socket> &workers, int i, std::vector<bool> &as
         auto [nsize, nstatus] = workers.at(i).recv(metadata_buff);
         size = nsize;
     }
-
-    // std::cout << "metadata received: " << size << std::endl;
+    
     const char *metadata_buff_data = reinterpret_cast<const char *>(metadata_buff.data());
 
     std::string img_metadata(metadata_buff_data);
 
-    //split string and assign values
+    //parse through returned metadata on processed image
     std::string delim = ",";
     std::string widthS = img_metadata.substr(0, img_metadata.find(delim));
     img_metadata.erase(0, img_metadata.find(delim) + delim.length());
@@ -51,22 +53,16 @@ void msg_recv(std::vector<kn::tcp_socket> &workers, int i, std::vector<bool> &as
     int op = std::stoi(opS);
     int id = std::stoi(idS);
 
-    // std::cout << "Here is the height: " << height << std::endl;
-    // std::cout << "Here is the width: " << width << std::endl;
-    // std::cout << "Here is the op: " << op << std::endl;
-    // std::cout << "Here is the id: " << id << std::endl;
-
     // for checking if received full image (actual == calculated)
     size_t actual_size = 0;
     size_t calculated_size = width * height * sizeof(unsigned char);
     unsigned char *received_img = new unsigned char[width * height];
 
+    //fetch the processed image slice from the worker and store it in vector
     kn::buffer<IMG_DATA_BUFF_SIZE> img_buff;
-
     while (actual_size < calculated_size)
     {
         auto [nsize, nstatus] = workers.at(i).recv(img_buff);
-        // std::cout << "size received: " << nsize << std::endl;
 
         auto buff_data = reinterpret_cast<unsigned char *>(img_buff.data());
 
@@ -82,10 +78,12 @@ void msg_recv(std::vector<kn::tcp_socket> &workers, int i, std::vector<bool> &as
 
 #pragma omp critical
     {
+        //change status of worker to available
         assigned.at(i) = false;
     }
 }
 
+//used to pad our metadata so that it fits the buffer perfectly on the worker end
 void metadata_padding(std::string &metadata)
 {
     size_t lenx = METADATA_MAX_LENGTH - metadata.length() - 1;
@@ -95,7 +93,7 @@ void metadata_padding(std::string &metadata)
     }
 }
 
-//Arguments order: image file name, image out file name, function name (blur, threshold, or upsample), chunks, threads, blur_size, upscale_size
+//Arguments order: image file name, image out file name, function name (blur 2, threshold 1, or upsample 3), chunks, threads, blur_size, upscale_size
 int main(int argc, char *argv[])
 {
 
@@ -109,13 +107,16 @@ int main(int argc, char *argv[])
     workers.push_back(kn::endpoint("127.0.0.1:3002"));
     workers.push_back(kn::endpoint("127.0.0.1:3003"));
 
+    //connect to each worker
     for (int i = 0; i < workers.size(); i++)
     {
         workers.at(i).connect();
     }
 
-    //Constants go here up top
+    //these values never change
     int n = 1;
+    
+    //the amount of chunks we have
     const int hotdogSections = std::stoi(argv[4]);
 
     // Array of processed images
@@ -125,7 +126,7 @@ int main(int argc, char *argv[])
     //Pop chunk off as it's sent
     std::vector<unsigned char *> chunks;
 
-    //TODO: Split up into chunks and push onto vector
+    
     int w;
     int h;
     int op = std::stoi(argv[3]);
@@ -134,6 +135,7 @@ int main(int argc, char *argv[])
     int sum = 0;
     int threads = std::stoi(argv[5]);
 
+    //Assign blur_size or upscale_size depending on the op code used
     if (op == 2)
     {
         blur_size = std::stoi(argv[6]);
@@ -143,10 +145,14 @@ int main(int argc, char *argv[])
         upscale_size = std::stoi(argv[6]);
     }
 
+    //load the specified image
     unsigned char *data = stbi_load(argv[1], &w, &h, &n, 1);
-    n = 1; // so that stbi_write_png works for greyscale
+    n = 1; // so that stbi_write_png works for greyscale (n changes after image loading)
 
+    //calculate the height of each chunk
     int chunk_height = h / hotdogSections;
+    
+    //slice up the image and store each slice on the vector
     for (int chunk = 0; chunk < hotdogSections; chunk++)
     {
 
@@ -166,11 +172,13 @@ int main(int argc, char *argv[])
         chunks.push_back(hotdogData);
     }
 
+    //to be used for padding purposes
     unsigned char *lastChunk = chunks.at(chunks.size() - 1);
     unsigned char *firstChunk = chunks.at(0);
 
     auto start = std::chrono::high_resolution_clock::now();
 
+    //keep sending work while there are still chunks/slices left to process
     while (chunks.size() != 0)
     {
 
@@ -180,15 +188,14 @@ int main(int argc, char *argv[])
         {
             #pragma omp critical
             {
+                //if worker is available we send it a slice to process
                 if (assigned.at(i) == false && chunks.size() > 0)
                 {
-
-                    // std::cout << "chunks left " << chunks.size() << std::endl;
-
                     int chunk_id = chunks.size() - 1;
-                    // printf("thread id %d chunk id %d\n", omp_get_thread_num(), chunk_id);
                     unsigned char *chunk = chunks.back();
                     chunks.pop_back();
+                    
+                    //set worker status to busy
                     assigned.at(i) = true;
 
                     //combine width, height, op code, id, threads in a string
@@ -197,7 +204,7 @@ int main(int argc, char *argv[])
                     if (op == 1)
                     {
 
-                        // add more metadata
+                        // add more metadata necessary for thresholding
                         img_metadata += std::to_string(sum) + "," + std::to_string(w) + "," + std::to_string(h);
                         metadata_padding(img_metadata);
 
@@ -208,15 +215,15 @@ int main(int argc, char *argv[])
                     }
                     else if (op == 2)
                     {
-                        //combine width, height, and op code in a string
+                        // add more metadata necessary for bluring
                         img_metadata += std::to_string(blur_size) + "," + std::to_string(hotdogSections) + ",";
                         metadata_padding(img_metadata);
 
-                        // std::cout << "Sending image metadata: " << img_metadata << std::endl;
                         workers.at(i).send(reinterpret_cast<const std::byte *>(img_metadata.c_str()), sizeof(unsigned char) * (img_metadata.length() + 1));
 
                         // send the data of the chunk
-                        // std::cout << "send data\n";
+                        
+                        //send chunk with bottom padding only
                         if (chunk_id == 0)
                         {
 
@@ -242,8 +249,8 @@ int main(int argc, char *argv[])
                             }
 
                             const auto [send_size, send_status] = workers.at(i).send(reinterpret_cast<const std::byte *>(paddedChunk), (w * chunk_height) + (w * (blur_size / 2)) * sizeof(unsigned char));
-                            // std::cout << "sent: " << send_size << ", " << (w * chunk_height) + (w * (blur_size / 2)) << std::endl;
                         }
+                        //send chunk with top padding only
                         else if (chunk_id == hotdogSections - 1)
                         {
 
@@ -269,8 +276,8 @@ int main(int argc, char *argv[])
                             }
 
                             const auto [send_size, send_status] = workers.at(i).send(reinterpret_cast<const std::byte *>(paddedChunk), (w * chunk_height) + (w * (blur_size / 2)) * sizeof(unsigned char));
-                            // std::cout << "sent: " << send_size << ", " << (w * chunk_height) + (w * (blur_size / 2)) << std::endl;
                         }
+                        //send chunk with both top and bottom padding
                         else
                         {
 
@@ -302,15 +309,14 @@ int main(int argc, char *argv[])
                             }
 
                             const auto [send_size, send_status] = workers.at(i).send(reinterpret_cast<const std::byte *>(paddedChunk), (w * chunk_height) + 2 * (w * (blur_size / 2)) * sizeof(unsigned char));
-                            // std::cout << "sent: " << send_size << ", " << (w * chunk_height) + (w * 2 * (blur_size / 2)) <<  std::endl;
                         }
                     }
                     else
                     {
+                        //add more metadata needed for upscale
                         img_metadata += std::to_string(upscale_size) + ",";
                         metadata_padding(img_metadata);
 
-                        // std::cout << "Sending image metadata: " << img_metadata << std::endl;
                         workers.at(i).send(reinterpret_cast<const std::byte *>(img_metadata.c_str()), sizeof(unsigned char) * (img_metadata.length() + 1));
 
                         // send the data of the chunk
@@ -324,12 +330,14 @@ int main(int argc, char *argv[])
         }
     }
 
+    //if image is being upscaled we need to re-adjust our width and chunk height accordingly
     if (op == 3)
     {
         w *= upscale_size;
         chunk_height *= upscale_size;
     }
 
+    
     size_t chunk_len = w * chunk_height;
     int result_h = chunk_height * hotdogSections;
     unsigned char *result = new unsigned char[w * result_h];
@@ -344,6 +352,7 @@ int main(int argc, char *argv[])
         }
     }
 
+    //write the image to the output destination
     stbi_write_png(argv[2], w, result_h, 1, result, w * 1);
 
     auto end = std::chrono::high_resolution_clock::now();
